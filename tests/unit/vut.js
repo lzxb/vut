6a3ee7bb078074240874dac56618848526f458c9
lzxb <1340641314@qq.com>
2017-10-26 11:16:24 +0800

    Fix test

--- tests/unit/vut.js
+++ tests/unit/vut.js
@@ -1,7 +1,22 @@
 import ava from 'ava'
 import Vut from '../../src/index'
 
-ava('ok', t => {
-  console.log(Vut)
-  t.pass()
+ava('base', t => {
+  const vut = new Vut()
+  vut.create('census', {
+    data () {
+      return {
+        count: 0,
+        arr: []
+      }
+    },
+    plus () {
+      this.count++
+    }
+  })
+  t.is(vut.store.census.count, 0)
+  t.is(vut.store.census.$state.count, 0)
+  t.deepEqual(vut.store.census.arr, [])
+  t.deepEqual(vut.store.census.$state.arr, [])
+  t.is(vut.store.census.arr, vut.store.census.$state.arr)
 })
