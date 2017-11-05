54038d89bc42da3f0c9fd828ed5d26241c27231d
lzxb <1340641314@qq.com>
2017-11-05 20:07:45 +0800

    Remove note

--- examples/base/vue/index.js
+++ examples/base/vue/index.js
@@ -9,33 +9,3 @@ export default new Vue({
     return h(App)
   }
 })
-
-// import Vut from 'vut'
-
-// const store = new Vut()
-
-// store.addModules('user', {
-//   vuts: ['user'],
-//   data () {
-//     return {
-//       count: 0
-//     }
-//   },
-//   plus () {
-//     this.count++
-//   },
-//   modules: {
-//     order: {
-//       data () {
-//         return {
-//           list: []
-//         }
-//       },
-//       load () {
-//         this.list.push(++this.list.length)
-//       }
-//     }
-//   }
-// })
-// console.log(store.getState({ user: 'user' }))
-// console.log(store.getState('user'))
